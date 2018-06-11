b6ce4d42ddd684330f06dfef41725ae47bf32bda
schueler <schueler@ubuntu>
2018-06-11 00:13:01 -0700

    3rd commit (panic)

---
+++ ForDeletion/ForDeletion/src/PkgGui/MainGUI.java
@@ -0,0 +1,31 @@
+package PkgGui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainGUI extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	
+	/**
+	 * Create the frame.
+	 */
+	public MainGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		this.setVisible(true);
+	}
+
+}
